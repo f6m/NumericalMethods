f60cfd452d4904bec4554f6bbfe5fb943736884b
f6m <mgf6mail@gmail.com>
2022-03-09 00:01:21 -0600

    Update mgs.py

--- algebralineal/mgs.py
+++ algebralineal/mgs.py
@@ -1,17 +1,43 @@
-# Aproximacion al sistema Ax=b mediante el método de Gauss-Seidel
-# Definicion de las matrices para el esquema iterativo de Gauss-Seidel
-import numpy
-D=numpy.array([[4,0,0],[0,7,0],[0,0,3]])
-Dinv=numpy.linalg.inv(D)
-Dinv
-array([[ 0.25      ,  0.        ,  0.        ],
-       [ 0.        ,  0.14285714,  0.        ],
-       [ 0.        ,  0.        ,  0.33333333]])
-L=numpy.array([[0,0,0],[-1,0,0],[1,-1,0]])
-U=numpy.array([[0,-3,0],[0,0,-1],[0,0,0]])
-b=numpy.array([1,2,3])
-x0=numpy.array([1,1,1])
+#Aproximacion al sistema Ax=b mediante el método de Gauss-Seidel
+#Definicion de las matrices para el esquema iterativo de Gauss-Seidel
+from numpy import *
+#preciciones
+d=0.0001
+e=0.0001
+N=100
+#descompocición para la matriz de coefs. y vector b, Ax=b
+A=array([[4,3,0],[1,7,1],[-1,1,3]])
+D=array([[4,0,0],[0,7,0],[0,0,3]])
+Dinv=linalg.inv(D)
+#Dinv
+#array([[ 0.25      ,  0.        ,  0.        ],
+#       [ 0.        ,  0.14285714,  0.        ],
+#       [ 0.        ,  0.        ,  0.33333333]])
+L=array([[0,0,0],[-1,0,0],[1,-1,0]])
+U=array([[0,-3,0],[0,0,-1],[0,0,0]])
+b=array([1,2,3])
 
-# Esquema iterativo de Gauss-Seidel, repetir
-x1=numpy.matmul(numpy.matmul(numpy.linalg.inv(D-L),U),x0)+numpy.matmul(numpy.linalg.inv(D-L),b)
+#vector inicial 
+x0=array([1,1,1])
 
+#primera iteración
+n=1
+x1=matmul(matmul(linalg.inv(D-L),U),x0)+matmul(linalg.inv(D-L),b)
+
+# Esquema iterativo de Jacobi, xn+1=(D-L)^-1Uxn+(D-L)^-1b
+while(n<N and linalg.norm(x1-x0) > e and linalg.norm(matmul(A,x1)-matmul(A,x0)) > d):
+	print(n,x1)
+	x0=x1
+	x1=matmul(matmul(linalg.inv(D-L),U),x0)+matmul(linalg.inv(D-L),b)
+	n=n+1
+
+#Comprobacion:
+
+print(matmul(A,x1))
+print(b)	
+#plot 3D
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
