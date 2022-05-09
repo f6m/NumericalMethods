40275498920eb1c5711ef2395b73600a8f9f3445
f6m <mgf6mail@gmail.com>
2022-05-09 00:50:48 -0500

    Update simpsoncompuesto.py

--- integration/simpsoncompuesto.py
+++ integration/simpsoncompuesto.py
@@ -11,21 +11,23 @@ j=0
 sum1 = 0
 sum2 = 0
 x0=a
-def f(x): cos(exp(x^2))
+def f(x): return 1/(sqrt(1-(sin(x)**2))) #K(k)
+
 
 #suma1
-while(j<=N/2)
+while(j<=N/2):
      sum1 = sum1 + f(x0+(2j-2)*h)
      j=j+1
 
 #suma2
 j=0
-while(j<=N/2)
+while(j<=N/2):
      sum2 = sum2 + f(x0+(2j-1)*h)
      j=j+1
 
 print(h/3*(f(a)+2*sum1+4*sum2+f(b)))
 
-
-   	
+from mpmath import *
+mp.dps = 25; mp.pretty = True
+print(ellipk(-1))
  
