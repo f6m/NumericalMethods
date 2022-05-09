0a0c3ecb6888a09395323b0bb26a41511f4682b7
f6m <mgf6mail@gmail.com>
2022-05-09 01:33:09 -0500

    Update simpsoncompuesto.py

--- integration/simpsoncompuesto.py
+++ integration/simpsoncompuesto.py
@@ -1,33 +1,31 @@
 #Integración numérica: método del trapecio compuesto
 
 from sympy import *
-
-x=Symbol('x')
-a=1
-b=10
+import math
+#x=Symbol('x')
+a=0
+b=math.pi/2
 N=10 #debe ser par
 h=(b-a)/N
-j=0
 sum1 = 0
 sum2 = 0
 x0=a
-def f(x): return 1/(sqrt(1-(sin(x)**2))) #K(k)
-
+def f(x):return (1/sqrt(1+math.sin(x)*math.sin(x))) 
 
 #suma1
+j=2
 while(j<=N/2):
-     sum1 = sum1 + f(x0+(2j-2)*h)
+     sum1 = sum1 + f(x0+(2*j-2)*h)
      j=j+1
 
 #suma2
-j=0
+j=1
 while(j<=N/2):
-     sum2 = sum2 + f(x0+(2j-1)*h)
+     sum2 = sum2 + f(x0+(2*j-1)*h)
      j=j+1
 
-print(h/3*(f(a)+2*sum1+4*sum2+f(b)))
+print(simplify((h/3)*(f(a)+2*sum1+4*sum2+f(b))))
 
 from mpmath import *
 mp.dps = 25; mp.pretty = True
 print(ellipk(-1))
- 
