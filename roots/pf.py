25f502e2d519a2d5590a40b47b2b356c4d3b95cc
f6m <mgf6mail@gmail.com>
2023-02-09 17:52:44 -0600

    Update pf.py

--- roots/pf.py
+++ roots/pf.py
@@ -1,28 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  1 04:50:14 2022
-
-@author: usuario
-"""
-
 from __future__ import division
-#from sympy import *
 import math
 from numpy import cos,sin #funciones cos, sin de numpy
 import numpy as np #numerical python como np
 import matplotlib.pyplot as plt
 
-#Num. max. de iteraciones n y tolerancias
+#Numero maximo de iteraciones n
+#Error en la aproximacion permitido: e(horizontal) y d (vertical)
 n=100
 e=0.0001
 d=0.0001
 
-#funcion a encontrar el cero f(x)=0
+#funcion a encontrar el punto fijo f(x)=x
 def func(x): 
   return 4+0.0333*sin(2*x)
 #return ((40 + 1.1474/(x*x))*(x - 0.03985)) - 0.08314*200
 
-#puntos iniciales
+#punto inicial
 x0=-1000 #inicial 1
 #x2=0.8 #inicial 2
 
@@ -36,12 +29,11 @@ x1=func(x0)
 x.append(x1)
 
 T=[0,1]
-print(T,x)
-
+#print(T,x)
 t=2
 while t <= n and np.abs(x1-x0) >= e and np.abs(func(x1)-func(x0)) >= d:
-    t=t+1
     T.append(t)
+    t=t+1
     x0=x1
     x1=func(x0)
     print(t,x1)
