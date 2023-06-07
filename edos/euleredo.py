7bf4913c7102e65d708cfd1f119a97667f3f90ce
f6m <mgf6mail@gmail.com>
2023-06-07 07:50:58 -0600

    Update euleredo.py
    Plot

--- edos/euleredo.py
+++ edos/euleredo.py
@@ -5,16 +5,25 @@ def x1(t,x): return math.sin(x*x) + math.cos(t*t)
 
 h=1/5
 t=0
+T=[t]
 x=1
+X=[x]
 
 print(t,x)
 xth=x+h*x1(t,x)
 
 while(t <= 2):
     x=xth
+    X.append(xth)
     t=t+h
+    T.append(t)
     print(t,x)
     xth=x+h*x1(t,x)
 
-""
+import matplotlib.pyplot as plt
+
+plt.plot(T, X)
+plt.xlabel('Tiempo')
+plt.ylabel('X')
+plt.show()
 
