9f2d158a432aab81e6e565754337215e48077183
f6m <mgf6mail@gmail.com>
2025-06-15 01:04:34 -0600

    Update euleredosistemas.py

--- edos/euleredosistemas.py
+++ edos/euleredosistemas.py
@@ -7,21 +7,21 @@ import numpy as np #numerical python como np
 #https://dev.to/bitproject/how-to-store-data-in-python-24a5
 
 
-h=1/5 #tamano de paso
+h=1/500 #tamano de paso
 t=0 #valor inicial del tiempo
-z=np.array([1,2]) #vector inicial en t=0
+z=np.array([1,0]) #vector inicial en t=0
 
 time=[t] #Guardamos el tiempo en una lista
 theta=[z[0]] #Guardamos el tiempo en una lista
 
 # vector en R^2 derivada
-def D(z): return np.array([z[1],-sin(z[0])])
+def D(z): return np.array([z[1],-9.8182*sin(z[0])])
 
 
 print(t,z)
 zth=z+h*D(z)
 
-while(t <= 5):
+while(t <= 20):
     z=zth
     t=t+h
     time.append(t) #Guardamos el tiempo en una lista
