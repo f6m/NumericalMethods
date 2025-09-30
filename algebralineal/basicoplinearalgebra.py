5e388caf4f03350ada901963775ccd938e0295bd
f6m <mgf6mail@gmail.com>
2025-09-30 00:33:55 -0600

    Update basicoplinearalgebra.py

--- algebralineal/basicoplinearalgebra.py
+++ algebralineal/basicoplinearalgebra.py
@@ -1,6 +1,21 @@
 from numpy import *
+#Matriz cuadrada
 A=array([[1,0,0,4],[7,1,0,3],[0,0,1,2],[1,1,2,2]])
 linalg.det(A)
 b=array([1,2,3,4])
 linalg.solve(A,b)
 A.dot(linalg.solve(A,b))
+
+#Matriz con mas ecuaciones que variables
+C=array([[1,1],[2,1],[0,1]])
+b=array([1,2,3])
+x,residuo,rango, s=linalg.lstsq(C,b,rcond=None)
+print(x)
+
+#Matriz con mas ecuaciones que variables
+D=array([[1,1,1],[0,1,2]])
+b=array([1,2])
+Ap=linalg.pinv(D)
+print(Ap)
+x=dot(Ap,b)
+print(x)
